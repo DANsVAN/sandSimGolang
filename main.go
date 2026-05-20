09e4cc0e9acf6edc46bbf5b5e7994b169483d32d
jordan_caldwell <d9furj6j@gmail.com>
2026-05-20 14:40:59 -0600

    got raylib working

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
-package sandsim
+package main
+
+import rl "github.com/gen2brain/raylib-go/raylib"
+
+func main() {
+	rl.InitWindow(800, 450, "raylib-go [core] example - basic window")
+	rl.SetTargetFPS(60)
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.RayWhite)
+		rl.DrawText("Congrats! You created your first window on Omarchy!", 190, 200, 20, rl.LightGray)
+		rl.EndDrawing()
+	}
+
+	rl.CloseWindow()
+}
